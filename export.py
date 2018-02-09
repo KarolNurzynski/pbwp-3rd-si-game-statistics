9ee623198c82240667e1da3a48f0c08b26c48466
Karol <m-a-l-p-a@o2.pl>
2018-02-09 15:41:23 +0100

    Printing.py: added input option; Export.py created which generates answears.txt file; answears.txt - file generated by export.py

--- export.py
+++ export.py
@@ -1,2 +1,18 @@
-
 # Export functions
+
+import reports
+
+answears=[
+reports.count_games("game_stat.txt"),
+reports.decide("game_stat.txt", 2311),
+reports.get_latest("game_stat.txt"),
+reports.count_by_genre("game_stat.txt", "First-person shooter"),
+reports.get_line_number_by_title("game_stat.txt", "Diablo III"),
+reports.sort_abc("game_stat.txt"),
+reports.get_genres("game_stat.txt"),
+reports.when_was_top_sold_fps("game_stat.txt"),
+]
+
+with open("answears.txt","w") as f:
+    for element in answears:
+        f.write(str(element)+"\n")
--- printing.py
+++ printing.py
@@ -4,13 +4,14 @@
 import reports
 
 # Print 1
-def count_games_print(file_name):
+def count_games_print():
+    file_name=input("Enter file name: ")
     print("Number of games in the file: {}".format(reports.count_games(file_name)))
 
-count_games_print("game_stat.txt")
-
 # Print 2
-def decide_print(file_name, year):
+def decide_print():
+    file_name=input("Enter file name: ")
+    year=input("Enter year: ")
     answear=reports.decide(file_name, year)
     if answear==True:
         answear="Yes"
@@ -18,44 +19,38 @@ def decide_print(file_name, year):
         answear="No"
     print("Is there a game from {0} year?: {1}".format(year, answear))
 
-decide_print("game_stat.txt", 2311)
-
 # Print 3
 
-def get_latest_print(file_name):
+def get_latest_print():
+    file_name=input("Enter file name: ")
     print("The latest game is: {}".format(reports.get_latest(file_name)))
 
-get_latest_print("game_stat.txt")
 
 # Print 4
-def count_by_genre_print(file_name, genre):
+def count_by_genre_print():
+    file_name=input("Enter file name: ")
+    genre=input("Enter genre: ")
     print("Number of games in {0} : {1}".format(genre, reports.count_by_genre(file_name, genre)))
-
-count_by_genre_print("game_stat.txt", "First-person shooter")
 
 # Print 5
 
-def get_line_number_by_title_print(file_name,title):
+def get_line_number_by_title_print():
+    file_name=input("Enter file name: ")
+    title=input("Enter title: ")
     print("Line number of game {0} is: {1}".format(title, reports.get_line_number_by_title(file_name, title)))
 
-get_line_number_by_title_print("game_stat.txt", "Diablo III")
-
 # Print 6
-def sort_abc_print(file_name):
+def sort_abc_print():
+    file_name=input("Enter file name: ")
     print("Alphabetical ordered list of titles: "+str(reports.sort_abc(file_name)))
-
-sort_abc_print("game_stat.txt")
-
 
 # Print 7
 
-def get_genres_print(file_name):
+def get_genres_print():
+    file_name=input("Enter file name: ")
     print("A list of the genres: {}".format(reports.get_genres(file_name)))
 
-get_genres_print("game_stat.txt")
-
 # Print 8
-def when_was_top_sold_fps_print(file_name):
+def when_was_top_sold_fps_print():
+    file_name=input("Enter file name: ")
     print("The release date of the top sold First-person shooter game: {}".format(reports.when_was_top_sold_fps(file_name)))
-
-when_was_top_sold_fps_print("game_stat.txt")
