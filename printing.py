1c67e99cc4947f21be28abd619825e2f6b46dc25
Karol <m-a-l-p-a@o2.pl>
2018-02-09 17:00:08 +0100

    added pep8 formatting

--- printing.py
+++ printing.py
@@ -4,53 +4,77 @@
 import reports
 
 # Print 1
+
+
 def count_games_print():
-    file_name=input("Enter file name: ")
-    print("Number of games in the file: {}".format(reports.count_games(file_name)))
+    file_name = input("Enter file name: ")
+    print("Number of games in the file: {}".format(
+        reports.count_games(file_name)))
 
 # Print 2
+
+
 def decide_print():
-    file_name=input("Enter file name: ")
-    year=input("Enter year: ")
-    answear=reports.decide(file_name, year)
-    if answear==True:
-        answear="Yes"
+    file_name = input("Enter file name: ")
+    year = input("Enter year: ")
+    answear = reports.decide(file_name, year)
+    if answear:
+        answear = "Yes"
     else:
-        answear="No"
+        answear = "No"
     print("Is there a game from {0} year?: {1}".format(year, answear))
 
 # Print 3
 
+
 def get_latest_print():
-    file_name=input("Enter file name: ")
+    file_name = input("Enter file name: ")
     print("The latest game is: {}".format(reports.get_latest(file_name)))
 
 
 # Print 4
 def count_by_genre_print():
-    file_name=input("Enter file name: ")
-    genre=input("Enter genre: ")
-    print("Number of games in {0} : {1}".format(genre, reports.count_by_genre(file_name, genre)))
+    file_name = input("Enter file name: ")
+    genre = input("Enter genre: ")
+    print(
+        "Number of games in {0} : {1}".format(
+            genre,
+            reports.count_by_genre(
+                file_name,
+                genre)))
 
 # Print 5
 
+
 def get_line_number_by_title_print():
-    file_name=input("Enter file name: ")
-    title=input("Enter title: ")
-    print("Line number of game {0} is: {1}".format(title, reports.get_line_number_by_title(file_name, title)))
+    file_name = input("Enter file name: ")
+    title = input("Enter title: ")
+    print(
+        "Line number of game {0} is: {1}".format(
+            title,
+            reports.get_line_number_by_title(
+                file_name,
+                title)))
 
 # Print 6
+
+
 def sort_abc_print():
-    file_name=input("Enter file name: ")
-    print("Alphabetical ordered list of titles: "+str(reports.sort_abc(file_name)))
+    file_name = input("Enter file name: ")
+    print("Alphabetical ordered list of titles: " +
+          str(reports.sort_abc(file_name)))
 
 # Print 7
 
+
 def get_genres_print():
-    file_name=input("Enter file name: ")
+    file_name = input("Enter file name: ")
     print("A list of the genres: {}".format(reports.get_genres(file_name)))
 
 # Print 8
+
+
 def when_was_top_sold_fps_print():
-    file_name=input("Enter file name: ")
-    print("The release date of the top sold First-person shooter game: {}".format(reports.when_was_top_sold_fps(file_name)))
+    file_name = input("Enter file name: ")
+    print("The release date of the top sold First-person shooter game: {}".format(
+        reports.when_was_top_sold_fps(file_name)))
--- reports.py
+++ reports.py
@@ -2,61 +2,67 @@
 
 # Function 1
 
+
 def count_games(file_name):
     with open(file_name, "r") as f:
         return len(f.readlines())
 
 # Function 2
 
+
 def decide(file_name, year):
     with open(file_name, "r") as f:
-        a=f.readlines()
-        string=" ".join(a)
-        if string.find(str(year))==-1:
+        a = f.readlines()
+        string = " ".join(a)
+        if string.find(str(year)) == -1:
             return False
         else:
             return True
 
 # Function 3
 
+
 def get_latest(file_name):
-    table=[]
+    table = []
     with open(file_name, "r") as f:
         for row in f:
             table.append(row.split("\t"))
-    table.sort(key=lambda x:x[2], reverse=True)
+    table.sort(key=lambda x: x[2], reverse=True)
     return table[0][0]
 
 # Function 4
 
+
 def count_by_genre(file_name, genre):
-    table=[]
-    number=0
+    table = []
+    number = 0
     with open(file_name, "r") as f:
         for row in f:
             table.append(row.split("\t"))
     for row in table:
-        if row[3]==genre:
-            number+=1
+        if row[3] == genre:
+            number += 1
     return number
 
 # Function 5
 
+
 def get_line_number_by_title(file_name, title):
-    table=[]
+    table = []
     with open(file_name, "r") as f:
         for row in f:
             table.append(row.split("\t"))
     for row in table:
         if title in row:
-            return table.index(row)+1
+            return table.index(row) + 1
     raise ValueError("There is no game {} in the list".format(title))
 
 # Function 6
 
+
 def sort_abc(file_name):
-    table=[]
-    titles=[]
+    table = []
+    titles = []
     sorted_titles = []
     with open(file_name, "r") as f:
         for row in f:
@@ -74,9 +80,10 @@ def sort_abc(file_name):
 
 # Function 7
 
+
 def get_genres(file_name):
-    table=[]
-    genres=[]
+    table = []
+    genres = []
     with open(file_name, "r") as f:
         for row in f:
             table.append(row.split("\t"))
@@ -86,16 +93,17 @@ def get_genres(file_name):
 
 # Function 8
 
+
 def when_was_top_sold_fps(file_name):
-    table=[]
-    fps=[]
+    table = []
+    fps = []
     with open(file_name, "r") as f:
         for row in f:
             table.append(row.split("\t"))
     for row in table:
-        if row[3]=="First-person shooter":
-            fps.append([float(row[1]),row[0], int(row[2])])
-    fps.sort(key=lambda x:x[0], reverse=True)
-    if len(fps)==0:
+        if row[3] == "First-person shooter":
+            fps.append([float(row[1]), row[0], int(row[2])])
+    fps.sort(key=lambda x: x[0], reverse=True)
+    if len(fps) == 0:
         raise ValueError
     return fps[0][2]
