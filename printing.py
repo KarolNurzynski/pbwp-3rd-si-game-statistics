0aa241642f92355ed0d621f7c0ad69f83bd7aa96
Karol <m-a-l-p-a@o2.pl>
2018-02-09 15:11:13 +0100

    8 functions and 8 printing functions added

--- printing.py
+++ printing.py
@@ -1,2 +1,61 @@
 
 # Printing functions
+
+import reports
+
+# Print 1
+def count_games_print(file_name):
+    print("Number of games in the file: {}".format(reports.count_games(file_name)))
+
+count_games_print("game_stat.txt")
+
+# Print 2
+def decide_print(file_name, year):
+    answear=reports.decide(file_name, year)
+    if answear==True:
+        answear="Yes"
+    else:
+        answear="No"
+    print("Is there a game from {0} year?: {1}".format(year, answear))
+
+decide_print("game_stat.txt", 2311)
+
+# Print 3
+
+def get_latest_print(file_name):
+    print("The latest game is: {}".format(reports.get_latest(file_name)))
+
+get_latest_print("game_stat.txt")
+
+# Print 4
+def count_by_genre_print(file_name, genre):
+    print("Number of games in {0} : {1}".format(genre, reports.count_by_genre(file_name, genre)))
+
+count_by_genre_print("game_stat.txt", "First-person shooter")
+
+# Print 5
+
+def get_line_number_by_title_print(file_name,title):
+    print("Line number of game {0} is: {1}".format(title, reports.get_line_number_by_title(file_name, title)))
+
+get_line_number_by_title_print("game_stat.txt", "Diablo III")
+
+# Print 6
+def sort_abc_print(file_name):
+    print("Alphabetical ordered list of titles: "+str(reports.sort_abc(file_name)))
+
+sort_abc_print("game_stat.txt")
+
+
+# Print 7
+
+def get_genres_print(file_name):
+    print("A list of the genres: {}".format(reports.get_genres(file_name)))
+
+get_genres_print("game_stat.txt")
+
+# Print 8
+def when_was_top_sold_fps_print(file_name):
+    print("The release date of the top sold First-person shooter game: {}".format(reports.when_was_top_sold_fps(file_name)))
+
+when_was_top_sold_fps_print("game_stat.txt")
--- reports.py
+++ reports.py
@@ -1,2 +1,100 @@
-
 # Report functions
+
+# Function 1
+
+def count_games(file_name):
+    with open(file_name, "r") as f:
+        return len(f.readlines())
+
+# Function 2
+
+def decide(file_name, year):
+    with open(file_name, "r") as f:
+        a=f.readlines()
+        string=" ".join(a)
+        if string.find(str(year))==-1:
+            return False
+        else:
+            return True
+
+# Function 3
+
+def get_latest(file_name):
+    table=[]
+    with open(file_name, "r") as f:
+        for row in f:
+            table.append(row.split("\t"))
+    table.sort(key=lambda x:x[2], reverse=True)
+    return table[0][0]
+
+# Function 4
+
+def count_by_genre(file_name, genre):
+    table=[]
+    number=0
+    with open(file_name, "r") as f:
+        for row in f:
+            table.append(row.split("\t"))
+    for row in table:
+        if row[3]==genre:
+            number+=1
+    return number
+
+# Function 5
+
+def get_line_number_by_title(file_name, title):
+    table=[]
+    with open(file_name, "r") as f:
+        for row in f:
+            table.append(row.split("\t"))
+    for row in table:
+        if title in row:
+            return table.index(row)+1
+    raise ValueError("There is no game {} in the list".format(title))
+
+# Function 6
+
+def sort_abc(file_name):
+    table=[]
+    titles=[]
+    with open(file_name, "r") as f:
+        for row in f:
+            table.append(row.split("\t"))
+    for row in table:
+        titles.append(row[0])
+    #element=0
+    for element in range(len(titles)):
+        for index in range(len(titles)):
+            if titles[element]<titles[index]:
+                titles.insert(index, titles[element])
+                titles.pop(element)
+                break
+    return titles
+
+# Function 7
+
+def get_genres(file_name):
+    table=[]
+    genres=[]
+    with open(file_name, "r") as f:
+        for row in f:
+            table.append(row.split("\t"))
+    for row in table:
+        genres.append(row[3])
+    return sorted(set(genres))
+
+# Function 8
+
+def when_was_top_sold_fps(file_name):
+    table=[]
+    fps=[]
+    with open(file_name, "r") as f:
+        for row in f:
+            table.append(row.split("\t"))
+    for row in table:
+        if row[3]=="First-person shooter":
+            fps.append([float(row[1]),row[0], int(row[2])])
+    fps.sort(key=lambda x:x[0], reverse=True)
+    if len(fps)==0:
+        raise ValueError
+    return fps[0][2]
